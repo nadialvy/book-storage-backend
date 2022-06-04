ea4fe83ff1c47a975b46d4517f3c8420ad58b5d1
nadialvy <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2022-06-04 21:58:15 +0700

    Add book

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const {
+    addBook,
+} = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBook,
+    },
+
+];
+
+module.exports = routes;
